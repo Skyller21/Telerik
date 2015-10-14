724008b540e33d369bdea0dc14eb7f4212914f00
vesheff <martin.veshev@gmail.com>
2015-10-15 00:22:58 +0300

    Mocking homework done

---
+++ 08.High Quality Code/20.Mocking/Cars.Tests.JustMock/Mocks/JustMockCarsRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Cars.Tests.JustMock.Mocks
+{
+    using System.Linq;
+
+    using Cars.Contracts;
+    using Cars.Models;
+
+    using Telerik.JustMock;
+
+    public class JustMockCarsRepository : CarRepositoryMock, ICarsRepositoryMock
+    {
+        protected override void ArrangeCarsRepositoryMock()
+        {
+            this.CarsData = Mock.Create<ICarsRepository>();
+            Mock.Arrange(() => this.CarsData.Add(Arg.IsAny<Car>())).DoInstead((Car car) => this.FakeCarCollection.Add(car));
+            Mock.Arrange(() => this.CarsData.All()).Returns(this.FakeCarCollection);
+            Mock.Arrange(() => this.CarsData.Search(Arg.AnyString)).Returns((string s) => this.FakeCarCollection
+                .Where(c => c.Make.ToLower() == s.ToLower() || c.Model.ToLower() == s.ToLower()).ToList());
+            Mock.Arrange(() => this.CarsData.GetById(Arg.AnyInt)).Returns((int id) => this.FakeCarCollection.FirstOrDefault(c => c.Id == id));
+            Mock.Arrange(() => this.CarsData.SortedByMake()).Returns(this.FakeCarCollection.OrderBy(c => c.Make).ToList());
+            Mock.Arrange(() => this.CarsData.SortedByYear()).Returns(this.FakeCarCollection.OrderBy(c => c.Year).ToList());
+        }
+    }
+}
